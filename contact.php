3d364900c3cfeabb11fe9cb14e50e7c500634c0b
NJittakorn <166985281+NJittakorn@users.noreply.github.com>
2025-01-08 09:36:18 +0700

    แก้ไข contact.php ครั้งที่ 3

--- contact.php
+++ contact.php
@@ -18,6 +18,7 @@
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
                 <li><a href="detail.php">รายละเอียด</li>
+                <li><a href="edit.php">แก้เพิ่มแล้วหล๋าวววว</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
